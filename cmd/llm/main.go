9a82ea5d42b07a902a88d18500ccfd5911e35de9
Casey Brooks <casey.brooks@agyn.io>
2026-03-12 18:27:16 +0000

    fix: register grpc health service

--- cmd/llm/main.go
+++ cmd/llm/main.go
@@ -21,6 +21,8 @@ import (
 	"github.com/agynio/llm/internal/proxy"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 const shutdownTimeout = 10 * time.Second
@@ -60,6 +62,9 @@ func run() error {
 	proxyService := proxy.NewService(resolver, &http.Client{})
 
 	grpcServer := grpc.NewServer()
+	healthServer := health.NewServer()
+	healthpb.RegisterHealthServer(grpcServer, healthServer)
+	healthServer.SetServingStatus("", healthpb.HealthCheckResponse_SERVING)
 	llmv1.RegisterLLMServiceServer(grpcServer, grpcserver.New(providerStore, modelStore, proxyService))
 
 	grpcListener, err := net.Listen("tcp", cfg.GRPCAddress)
